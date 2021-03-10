14dcb6f6e3aad82e787003bba3a53d4d7c1fc212
Gnutyud <nguyeduytungtd3@gmail.com>
2021-03-10 23:07:30 +0700

    update project list

--- Js/renderProjectHtml.js
+++ Js/renderProjectHtml.js
@@ -9,7 +9,7 @@ const project = [{
         alt: "project1",
         category: "landing-page",
         name: "Google",
-        desc: "Google search page"
+        desc: "Google search page",
     },
     {
         id: 2,
@@ -18,16 +18,16 @@ const project = [{
         alt: "project2",
         category: "landing-page",
         name: "Javascript",
-        desc: "Javascript documentation landing page"
+        desc: "Javascript documentation landing page",
     },
     {
         id: 3,
-        link: "https://gnutyud.github.io/Project-3---FFC/index.html",
-        image: "./images/projects/Netflix.png",
+        link: "https://gnutyud.github.io/Calculator/",
+        image: "./images/projects/Calculator.png",
         alt: "project3",
-        category: "landing-page",
-        name: "Netflix",
-        desc: "Just used HTML&CSS to clone it"
+        category: "javascript",
+        name: "Calculator",
+        desc: "Just a simple calculator inspired from MS Calculator",
     },
     {
         id: 4,
@@ -36,23 +36,28 @@ const project = [{
         alt: "project4",
         category: "landing-page",
         name: "Survey form",
-        desc: "The first form page that I've made"
-    }
+        desc: "The first form page that I've made",
+    },
 ];
 
 function renderProject(arr) {
-    let html = '';
+    let html = "";
     for (var i = 0; i < project.length; i++) {
-        html += '<a href="' + project[i].link + '" class="project-item-inner shadow-dark" data-category="' + project[i].category + '" target="_blank">';
+        html +=
+            '<a href="' +
+            project[i].link +
+            '" class="project-item-inner shadow-dark" data-category="' +
+            project[i].category +
+            '" target="_blank">';
         html += '<div class="project-img">';
         html += '<img src="' + project[i].image + '" alt="' + project[i].alt + '">';
-        html += '</div>';
+        html += "</div>";
         html += '<div class="project-info">';
-        html += '<h4>' + project[i].name + '</h4>';
-        html += '<p>' + project[i].desc + '</p>';
-        html += '</div>';
-        html += '</a>';
-    };
-    document.getElementById('project-item').innerHTML = html;
+        html += "<h4>" + project[i].name + "</h4>";
+        html += "<p>" + project[i].desc + "</p>";
+        html += "</div>";
+        html += "</a>";
+    }
+    document.getElementById("project-item").innerHTML = html;
 }
 renderProject(project);
