1af26011f43a51e2269bc91bd95bb9a2be862a52
Nguyễn Duy Tùng <73052334+Gnutyud@users.noreply.github.com>
2021-04-15 23:17:51 +0700

    update a new project

--- Js/main.js
+++ Js/main.js
@@ -1,80 +1,81 @@
 // projects filter items
-const filterContainer = document.querySelector(".project-filter"),
-    filterBtn = filterContainer.children,
-    totalFilterBtn = filterBtn.length,
-    projectItems = document.querySelectorAll(".project-item-inner"),
-    totalProjectItem = projectItems.length;
+const filterContainer = document.querySelector('.project-filter'),
+  filterBtn = filterContainer.children,
+  totalFilterBtn = filterBtn.length,
+  projectItems = document.querySelectorAll('.project-item-inner'),
+  totalProjectItem = projectItems.length;
 for (let i = 0; i < totalFilterBtn; i++) {
-    filterBtn[i].addEventListener("click", function() {
-        filterContainer.querySelector(".active").classList.remove("active");
-        this.classList.add("active");
+  filterBtn[i].addEventListener('click', function () {
+    filterContainer.querySelector('.active').classList.remove('active');
+    this.classList.add('active');
 
-        const filterValue = this.getAttribute("data-filter");
-        for (let j = 0; j < totalProjectItem; j++) {
-            if (filterValue === projectItems[j].getAttribute("data-category")) {
-                projectItems[j].classList.add("show");
-                projectItems[j].classList.remove("hide");
-            } else {
-                projectItems[j].classList.add("hide");
-                projectItems[j].classList.remove("show");
-            }
-            if (filterValue === "all") {
-                projectItems[j].classList.add("show");
-                projectItems[j].classList.remove("hide");
-            }
-        }
-    });
+    const filterValue = this.getAttribute('data-filter');
+    for (let j = 0; j < totalProjectItem; j++) {
+      if (filterValue === projectItems[j].getAttribute('data-category')) {
+        projectItems[j].classList.add('show');
+        projectItems[j].classList.remove('hide');
+      } else {
+        projectItems[j].classList.add('hide');
+        projectItems[j].classList.remove('show');
+      }
+      if (filterValue === 'all') {
+        projectItems[j].classList.add('show');
+        projectItems[j].classList.remove('hide');
+      }
+    }
+  });
 }
 // active nav menu buttons
-const navMenu = document.querySelector(".nav-menu"),
-    navMenuBtn = navMenu.getElementsByClassName("nav-menu-link"),
-    section = document.querySelectorAll(".section");
+const navMenu = document.querySelector('.nav-menu'),
+  navMenuBtn = navMenu.getElementsByClassName('nav-menu-link'),
+  section = document.querySelectorAll('.section');
 for (var m = 0; m < navMenuBtn.length; m++) {
-    navMenuBtn[m].addEventListener("click", function() {
-        navMenu.querySelector(".active").classList.remove("active");
-        this.classList.add("active");
-        showSection(this);
-    })
+  navMenuBtn[m].addEventListener('click', function () {
+    navMenu.querySelector('.active').classList.remove('active');
+    this.classList.add('active');
+    showSection(this);
+  });
 }
 
 function showSection(el) {
-    const href = el.getAttribute("href").split("#")[1];
-    for (var i = 0; i < section.length; i++) {
-        section[i].classList.add("hidden");
-    }
-    document.querySelector("#" + href).classList.remove("hidden");
+  const href = el.getAttribute('href').split('#')[1];
+  for (var i = 0; i < section.length; i++) {
+    section[i].classList.add('hidden');
+  }
+  document.querySelector('#' + href).classList.remove('hidden');
 }
 // switcher skin color
-const links = document.getElementsByClassName("color-link");
+const links = document.getElementsByClassName('color-link');
 
 function swichColor(color) {
-    for (var i = 0; i < links.length; i++) {
-        if (color === links[i].getAttribute("title")) {
-            links[i].removeAttribute("disabled");
-        } else {
-            links[i].setAttribute("disabled", "true");
-        }
+  for (var i = 0; i < links.length; i++) {
+    if (color === links[i].getAttribute('title')) {
+      links[i].removeAttribute('disabled');
+    } else {
+      links[i].setAttribute('disabled', 'true');
     }
+  }
 }
 // setting btn toggle
-document.querySelector(".toggle-setting").addEventListener("click", function() {
-        document.querySelector(".skin-body-box").classList.toggle("open");
-    })
-    // nav toggler
-document.querySelector(".nav-toggler").addEventListener("click", function() {
-        document.querySelector(".navside").classList.toggle("open");
-        document.querySelector(".nav-toggler").classList.toggle("open");
-        document.querySelector(".main-content").classList.toggle("open");
-    })
-    // darkmode click
-const darkmode = document.querySelectorAll(".darkmode-style");
+document
+  .querySelector('.toggle-setting')
+  .addEventListener('click', function () {
+    document.querySelector('.skin-body-box').classList.toggle('open');
+  });
+// nav toggler
+document.querySelector('.nav-toggler').addEventListener('click', function () {
+  document.querySelector('.navside').classList.toggle('open');
+  document.querySelector('.nav-toggler').classList.toggle('open');
+  document.querySelector('.main-content').classList.toggle('open');
+});
+// darkmode click
+const darkmode = document.querySelectorAll('.darkmode-style');
 for (var i = 0; i < darkmode.length; i++) {
-    darkmode[i].addEventListener("change", function() {
-        if (this.value === "dark") {
-            document.body.className = "dark";
-        } else {
-            document.body.className = "";
-        }
-    })
-
+  darkmode[i].addEventListener('change', function () {
+    if (this.value === 'dark') {
+      document.body.className = 'dark';
+    } else {
+      document.body.className = '';
+    }
+  });
 }
--- Js/renderProjectHtml.js
+++ Js/renderProjectHtml.js
@@ -2,62 +2,72 @@ let perPage = 8;
 let idPage = 1;
 let start = 0;
 let end = perPage;
-const project = [{
-        id: 1,
-        link: "https://gnutyud.github.io/Google/#",
-        image: "./images/projects/Google.png",
-        alt: "project1",
-        category: "landing-page",
-        name: "Google",
-        desc: "Google search page",
-    },
-    {
-        id: 2,
-        link: "https://gnutyud.github.io/Project-4---FFC/",
-        image: "./images/projects/Jsdocument.png",
-        alt: "project2",
-        category: "landing-page",
-        name: "Javascript",
-        desc: "Javascript documentation landing page",
-    },
-    {
-        id: 3,
-        link: "https://gnutyud.github.io/Calculator/",
-        image: "./images/projects/Calculator.png",
-        alt: "project3",
-        category: "javascript",
-        name: "Calculator",
-        desc: "Just a simple calculator inspired from MS Calculator",
-    },
-    {
-        id: 4,
-        link: "https://gnutyud.github.io/Project-2-Survey-form/",
-        image: "./images/projects/Survey Form.png",
-        alt: "project4",
-        category: "landing-page",
-        name: "Survey form",
-        desc: "The first form page that I've made",
-    },
+const project = [
+  {
+    id: 1,
+    link: 'https://gnutyud.github.io/Google/#',
+    image: './images/projects/Google.png',
+    alt: 'project1',
+    category: 'landing-page',
+    name: 'Google',
+    desc: 'Google search page',
+  },
+  {
+    id: 2,
+    link: 'https://gnutyud.github.io/Project-4---FFC/',
+    image: './images/projects/Jsdocument.png',
+    alt: 'project2',
+    category: 'landing-page',
+    name: 'Javascript',
+    desc: 'Javascript documentation landing page',
+  },
+  {
+    id: 3,
+    link: 'https://gnutyud.github.io/Calculator/',
+    image: './images/projects/Calculator.png',
+    alt: 'project3',
+    category: 'javascript',
+    name: 'Calculator',
+    desc: 'Just a simple calculator inspired from MS Calculator',
+  },
+  {
+    id: 4,
+    link: 'https://gnutyud.github.io/Project-2-Survey-form/',
+    image: './images/projects/Survey Form.png',
+    alt: 'project4',
+    category: 'landing-page',
+    name: 'Survey form',
+    desc: "The first form page that I've made",
+  },
+  {
+    id: 5,
+    link: 'https://we-keep.herokuapp.com/',
+    image: './images/projects/we-keeper.png',
+    alt: 'We-Keeper',
+    category: 'javascript',
+    name: 'We Keeper',
+    desc: 'This is like a Google keep clone using Reactjs to built',
+  },
 ];
 
 function renderProject(arr) {
-    let html = "";
-    for (var i = 0; i < project.length; i++) {
-        html +=
-            '<a href="' +
-            project[i].link +
-            '" class="project-item-inner shadow-dark" data-category="' +
-            project[i].category +
-            '" target="_blank">';
-        html += '<div class="project-img">';
-        html += '<img src="' + project[i].image + '" alt="' + project[i].alt + '">';
-        html += "</div>";
-        html += '<div class="project-info">';
-        html += "<h4>" + project[i].name + "</h4>";
-        html += "<p>" + project[i].desc + "</p>";
-        html += "</div>";
-        html += "</a>";
-    }
-    document.getElementById("project-item").innerHTML = html;
+  let html = '';
+  for (var i = 0; i < project.length; i++) {
+    html +=
+      '<a href="' +
+      project[i].link +
+      '" class="project-item-inner shadow-dark" data-category="' +
+      project[i].category +
+      '" target="_blank">';
+    html += '<div class="project-img">';
+    html += '<img src="' + project[i].image + '" alt="' + project[i].alt + '">';
+    html += '</div>';
+    html += '<div class="project-info">';
+    html += '<h4>' + project[i].name + '</h4>';
+    html += '<p>' + project[i].desc + '</p>';
+    html += '</div>';
+    html += '</a>';
+  }
+  document.getElementById('project-item').innerHTML = html;
 }
 renderProject(project);
